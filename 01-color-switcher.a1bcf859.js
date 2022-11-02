23fe4a877f6054b75321c3d86c275e2dc2634a28
i-havr <106404009+i-havr@users.noreply.github.com>
2022-11-02 10:16:25 +0000

    Deploying to gh-pages from @ i-havr/goit-js-hw-09@899d991c1625e099f8e27aa460e18f01d9c3d789 🚀

--- 01-color-switcher.a1bcf859.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function d(){e.setAttribute("disabled","disabled"),t.removeAttribute("disabled")}function r(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0").toUpperCase()}`}t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),e.removeAttribute("disabled"),r(),timerId=setInterval(r,1e3)})),e.addEventListener("click",(function(){d(),clearInterval(timerId)})),d();
-//# sourceMappingURL=01-color-switcher.a1bcf859.js.map
